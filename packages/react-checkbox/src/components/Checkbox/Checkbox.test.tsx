117acc735d2d30480a837854dd2ce982f86e5947
Esteban Munoz <esteban.230@hotmail.com>
2021-08-06 16:34:23 -0600

    updating tests

--- packages/react-checkbox/src/components/Checkbox/Checkbox.test.tsx
+++ packages/react-checkbox/src/components/Checkbox/Checkbox.test.tsx
@@ -1,31 +1,16 @@
 import * as React from 'react';
 import { render, RenderResult } from '@testing-library/react';
 import { Checkbox } from './Checkbox';
-import { mount, ReactWrapper } from 'enzyme';
 import { isConformant } from '../../common/isConformant';
 import { resetIdsForTests } from '@fluentui/react-utilities';
 
-// TODO: add more tests here, and create visual regression tests in /apps/vr-tests
+// TODO: create visual regression tests in /apps/vr-tests
 
 describe('Checkbox', () => {
-  let component: ReactWrapper | undefined;
   let renderedComponent: RenderResult | undefined;
-  let checkboxRef: React.RefObject<HTMLInputElement> = React.createRef<HTMLInputElement>();
 
   beforeEach(() => {
     resetIdsForTests();
-  });
-
-  afterEach(() => {
-    checkboxRef = React.createRef<HTMLInputElement>();
-    if (component) {
-      component.unmount();
-      component = undefined;
-    }
-    if (renderedComponent) {
-      renderedComponent.unmount();
-      renderedComponent = undefined;
-    }
   });
 
   isConformant({
@@ -34,113 +19,101 @@ describe('Checkbox', () => {
   });
 
   it('renders a default state', () => {
-    renderedComponent = render(<Checkbox label="Default Checkbox" />);
+    renderedComponent = render(<Checkbox>Default Checkbox</Checkbox>);
     expect(renderedComponent.container).toMatchSnapshot();
   });
 
   it('renders unchecked correctly', () => {
-    renderedComponent = render(<Checkbox label="Default Checkbox" input={{ ref: checkboxRef }} />);
+    renderedComponent = render(<Checkbox>Default Checkbox</Checkbox>);
     expect(renderedComponent).toMatchSnapshot();
   });
 
   it('renders checked correctly', () => {
-    renderedComponent = render(<Checkbox checked label="Default Checkbox" />);
+    renderedComponent = render(<Checkbox checked>Default Checkbox</Checkbox>);
     expect(renderedComponent).toMatchSnapshot();
   });
 
   it('renders mixed correctly', () => {
-    renderedComponent = render(<Checkbox checked="mixed" label="Default Checkbox" />);
+    renderedComponent = render(<Checkbox checked="mixed">Default Checkbox</Checkbox>);
+    expect(renderedComponent).toMatchSnapshot();
+  });
+
+  it('renders circular correctly', () => {
+    renderedComponent = render(<Checkbox circular>Default Checkbox</Checkbox>);
     expect(renderedComponent).toMatchSnapshot();
   });
 
   it('respects id prop', () => {
-    component = mount(<Checkbox aria-describedby="descriptionID" id="checkbox" label="Default Checkbox" />);
-    expect(component.find('input').prop('id')).toEqual('checkbox');
+    renderedComponent = render(<Checkbox id="checkbox">Default Checkbox</Checkbox>);
+    expect(renderedComponent.getByRole('checkbox').id).toEqual('checkbox');
   });
 
   it('defaults to unchecked non-mixed', () => {
-    component = mount(<Checkbox label="Default Checkbox" input={{ ref: checkboxRef }} />);
+    const checkboxRef = React.createRef<HTMLInputElement>();
+    renderedComponent = render(<Checkbox input={{ ref: checkboxRef }}>Default Checkbox</Checkbox>);
 
-    const input = component.find('input');
-    expect(input.prop('checked')).toBe(false);
+    expect(renderedComponent.getByRole('checkbox').getAttribute('checked')).toBeNull();
     expect(checkboxRef.current?.checked).toBe(false);
   });
 
   it('respects defaultChecked prop', () => {
-    component = mount(<Checkbox defaultChecked input={{ ref: checkboxRef }} />);
+    let checkboxRef = React.createRef<HTMLInputElement>();
+    renderedComponent = render(
+      <Checkbox defaultChecked input={{ ref: checkboxRef }}>
+        Default Checkbox
+      </Checkbox>,
+    );
 
-    let input = component.find('input');
-    expect(input.prop('checked')).toBe(true);
+    expect(renderedComponent.getByRole('checkbox').getAttribute('checked')).not.toBeNull();
     expect(checkboxRef.current?.checked).toBe(true);
+    expect(checkboxRef.current?.indeterminate).toBe(false);
 
-    component.unmount();
+    renderedComponent.unmount();
     checkboxRef = React.createRef<HTMLInputElement>();
-    component = mount(<Checkbox defaultChecked="mixed" input={{ ref: checkboxRef }} />);
+    renderedComponent = render(
+      <Checkbox defaultChecked="mixed" input={{ ref: checkboxRef }}>
+        Default Checkbox
+      </Checkbox>,
+    );
 
-    input = component.find('input');
-    expect(input.prop('checked')).toBe(false);
+    expect(renderedComponent.getByRole('checkbox').getAttribute('checked')).toBeNull();
     expect(checkboxRef.current?.checked).toBe(false);
-  });
-
-  it('ignores defaulChecked updates', () => {
-    component = mount(<Checkbox defaultChecked input={{ ref: checkboxRef }} />);
-    component.setProps({ defaultChecked: false });
-    expect(component.find('input').prop('checked')).toBe(true);
-    expect(checkboxRef.current?.checked).toBe(true);
-
-    component.unmount();
-    checkboxRef = React.createRef<HTMLInputElement>();
-    component = mount(<Checkbox input={{ ref: checkboxRef }} />);
-    component.setProps({ defaultChecked: true });
-    expect(component.find('input').prop('checked')).toBe(false);
-    expect(checkboxRef.current?.checked).toBe(false);
+    expect(checkboxRef.current?.indeterminate).toBe(true);
   });
 
   it('respects checked prop', () => {
-    component = mount(<Checkbox checked input={{ ref: checkboxRef }} />);
+    let checkboxRef = React.createRef<HTMLInputElement>();
+    renderedComponent = render(
+      <Checkbox checked input={{ ref: checkboxRef }}>
+        Default Checkbox
+      </Checkbox>,
+    );
 
-    let input = component.find('input');
-    expect(input.prop('checked')).toBe(true);
+    expect(renderedComponent.getByRole('checkbox').getAttribute('checked')).not.toBeNull();
     expect(checkboxRef.current?.checked).toBe(true);
 
-    component.unmount();
+    renderedComponent.unmount();
     checkboxRef = React.createRef<HTMLInputElement>();
-    component = mount(<Checkbox checked="mixed" input={{ ref: checkboxRef }} />);
+    renderedComponent = render(
+      <Checkbox checked="mixed" input={{ ref: checkboxRef }}>
+        Default Checkbox
+      </Checkbox>,
+    );
 
-    input = component.find('input');
-    expect(input.prop('checked')).toBe(false);
+    expect(renderedComponent.getByRole('checkbox').getAttribute('checked')).toBeNull();
     expect(checkboxRef.current?.checked).toBe(false);
-  });
-
-  it('respects checked updates', () => {
-    component = mount(<Checkbox checked input={{ ref: checkboxRef }} />);
-    component.setProps({ checked: false });
-
-    let input = component.find('input');
-    expect(input.prop('checked')).toBe(false);
-    expect(checkboxRef.current?.checked).toBe(false);
-
-    component.setProps({ checked: 'mixed' });
-    input = component.find('input');
-    expect(input.prop('checked')).toBe(false);
-    expect(checkboxRef.current?.checked).toBe(false);
-  });
-
-  it("doesn't remove controlled mixed when no onChange provided", () => {
-    component = mount(<Checkbox checked="mixed" input={{ ref: checkboxRef }} />);
-    let input = component.find('input');
-    expect(checkboxRef.current?.indeterminate).toEqual(true);
-
-    input.simulate('change');
-
-    input = component.find('input');
-    expect(checkboxRef.current?.indeterminate).toEqual(true);
   });
 
   it('correctly sets indeterminate state through javascript', () => {
-    component = mount(<Checkbox defaultChecked="mixed" input={{ ref: checkboxRef }} />);
-    const input = component.find('input');
-    expect(input.prop('checked')).toBe(false);
+    const checkboxRef = React.createRef<HTMLInputElement>();
+    renderedComponent = render(
+      <Checkbox defaultChecked="mixed" input={{ ref: checkboxRef }}>
+        Default Checkbox
+      </Checkbox>,
+    );
+
+    expect(renderedComponent.getByRole('checkbox').getAttribute('checked')).toBeNull();
+    expect(checkboxRef.current?.checked).toEqual(false);
     expect(checkboxRef.current?.indeterminate).toEqual(true);
   });
 });
